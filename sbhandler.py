a704319e84ca2c19d2dd43fda04847b6f7ad2a57
David Bearden <dbeardene@gmail.com>
2022-06-10 11:53:59 -0400

    change DNA_SeqBlocks() to acct. for barcode

--- sbhandler.py
+++ sbhandler.py
@@ -1,15 +1,15 @@
-# ----------------------------------------------------------------------------------
-# Functions to handle the unique attributes of the consensus1.mat file in the Gong Lab
-# @author: davidebear
-# 
-# Note: Refer to https://github.com/emiliosalazar/matToPython/blob/main/MatFileMethods.py for a more encompassing MatLabLoader
-# ----------------------------------------------------------------------------------
+
+# from sbhandler import *
+from math import pow    
+# from fastai.vision.all import *
 
 # Needed imports
 import numpy as np
-from math import pow
 import h5py
 
+STOP_CODON = np.array([84, 65, 65, 84, 65, 71]) # T A A T A G
+
+# Source code for functions from seqblock_handler.py
 def load_matlab_file(mat_file, variable_name): # Note the double return statement
     """_summary_
     //UNIQUE: Only one variable for this .mat file
@@ -25,13 +25,12 @@ def load_matlab_file(mat_file, variable_name): # Note the double return statemen
     data = h5py_object.get(variable_name)
     data = np.array(data)
     return h5py_object, data
-
-class DNA_SeqBlocks(): # Loaded mat file that serves as a generator of seqblocks and ultimately CleanSeqBlock objects
-    """_summary_
+class DNA_SeqBlocks():
+    """Mat dataset 
     Loads the mat file with DNA sequences for easy iteration through all stored DNA sequence lists of varying coverage
     """
     def __init__(self, h5py_object, data):
-        """_summary_
+        """Initialize DNA_SeqBlocks() object
         Args:
             h5py_object (h5py_object): first return variable of load_matlab_file
             data (np.array): second return variable of load_matlab_file
@@ -39,58 +38,163 @@ class DNA_SeqBlocks(): # Loaded mat file that serves as a generator of seqblocks
         self.h5py_object = h5py_object
         self.data = np.transpose(data)
         self.size = len(data) 
-    def get_seqblock(self, number): # //TODO: make sure the default args work
-        """_summary_
-        Generates a seqblock and transposes the result. mat files store matrices in their transpose form
+        
+    def get_seqblock(self, number): # checked
+        x = self.h5py_object[self.data[0, number-1]][:, :] # //UNIQUE: 0th column because .mat file, align3 variable only has one row
+        return np.transpose(x)
+    
+    def get_coverage_count(self, number):
+        """Coverage count
 
         Args:
-            number (int): Which of the over 300,000 (self.size) seqblocks do you want? Input with MATLAB indexing
+            number (int): which sequence? matlab idx
 
         Returns:
-            np.array object: An unclean seqblock with dtype='uint8'. Further procesing to be done in seqblock_parser and into CleanSeqBlock object
+            int: Number of reads
         """
-        x = self.h5py_object[self.data[number-1, 0]][:, :] # //UNIQUE: 0th column because .mat file, align3 variable only has one row
-        return np.transpose(x)
-    def get_coverage_count(self, number):
         x = self.h5py_object[self.data[0, number-1]][0,:]
-        x = int((len(x)-4)/2)
-        return x
+        coverage_count = int((len(x)-4)/2)
+        return coverage_count
     
-#//TODO: Create a container of all seqblocks for readability (char arrays). Change seqblock_parser to an int4 array.
-# Do I need to make each of the four bases a number like [0.00, 0.33, 0.66, 0.99] to normalize everything?
+    def get_barcode(self, number): #//TODO
+        """Barcode finder
+
+        Args:
+            number (int): which sequence? matlab indexing
+
+        Returns:
+            barcode, true_len: returns barcode uint16 ndarray, last matlab idx of stop codon
+        """
+        x = self.h5py_object[self.data[0, number-1]][:, 1]
+        barcode = ""
+        true_len = 0
+        total = x.size
+        
+        idx = 0
+        search_stop=True
+        
+        while idx < total and search_stop: # To identify the stop codon
+            if (np.all(x[idx:idx+6] == STOP_CODON)):  # Translates to 'TAATAG'
+                true_len = idx+6
+                search_stop = False
+            idx += 1
+            
+        barcode = x[true_len:total]
+            
+        return barcode, true_len
+    
+    def get_quality(self, number, nuc_pos, coverage_count=0): # checked
+        if coverage_count == 0:
+            cov = self.get_coverage_count(number)
+            x = self.h5py_object[self.data[0, number-1]][nuc_pos, (1+cov):(1+2*cov)]
+            return np.transpose(x)
+        else:
+            x = self.h5py_object[self.data[0, number-1]][nuc_pos, (1+coverage_count):(1+2*coverage_count)]
+            return np.transpose(x)
+    
+    def get_nuc(self, number, nuc_pos, coverage_count=0): # checked
+        if coverage_count == 0:
+            x = (self.h5py_object[self.data[0,number-1]][nuc_pos-1, 1:self.get_coverage_count(number)+1])
+            return np.transpose(x)
+        else: 
+            x = self.h5py_object[self.data[0,number-1]][nuc_pos-1, 1:(coverage_count+1)]
+            return np.transpose(x)
+    
+    def get_interp_changes(self, number, coverage_count=0): # checked
+        if coverage_count == 0:
+            x = self.h5py_object[self.data[0,number-1]][:,(1+2*self.get_coverage_count(number))]
+            return np.transpose(x)
+        else:
+            x = self.h5py_object[self.data[0,number-1]][:,(1+2*coverage_count)]
+            return np.transpose(x)
+    
+    def get_interp_mutations(self, number, coverage_count=0):
+        if coverage_count == 0:
+            x = self.h5py_object[self.data[0,number-1]][:,(3+2*self.get_coverage_count(number))]
+            return np.transpose(x)
+        else:
+            x = self.h5py_object[self.data[0,number-1]][:,(3+2*coverage_count)]     
+            return np.transpose(x)      
+    
+    def get_interp_consensus(self, number, coverage_count=0):
+        if coverage_count == 0:
+            x = self.h5py_object[self.data[0,number-1]][:,(2+2*self.get_coverage_count(number))]
+            return np.transpose(x) 
+        else:
+            x = self.h5py_object[self.data[0,number-1]][:,(2+2*coverage_count)]
+            return np.transpose(x)
+ 
+    
 def seqblock_parser(seqblock):
-    """_summary_
-
-    Args:
-        seqblock (DNA_SeqBlocks.get_seqblock(number=)): A seqblock generated from the DNA_SeqBlocks dataset. Chooses a specified seqblock
-
-    Returns:
-        CleanSeqBlock object: Contains char arrays of cleaned aspects of each seqblock (target sequence, reads, quality, interpretations (3 types)).
-        Useable data for training, validation, testing.
-    """
     seqblock_parsed = CleanSeqBlock()
     total_columns = len(np.transpose(seqblock)) # is there a more efficient way?
     total_rows = len(seqblock)
     number_of_reads = int((total_rows - 4)/2) # last three rows have intreptations, first row is non-mutated target sequence, and N quality scores for N reads
     
-    seqblock_parsed.reads_count = number_of_reads;
+    seqblock_parsed.cov = number_of_reads;
     seqblock_parsed.len = total_columns;
     
-    for i in range(0,total_rows):
+    # print(total_columns)
+    # print(total_rows)
+    tag = ""
+    tag_length = 10_000
+    
+    for i in range(total_rows):
         # print(f" The sequenceblock input {seqblock}") #debug
         # print(f" The m x n size of the seqblock {seqblock.shape}") #debug
         # print(f" The ith row of the seqblock {current_seq}") #debug
         # print(f" The m x n size of the sequence {current_seq.shape}") #debug
-        new_format = ""
+        
+        # new_format = np.chararray(total_columns) //CHANGED to string
+        stop_found = False
+        # new_format[:] = 'q' #debug
         
         # print(f" The 0th entry of the initialized char array {new_format[0, 0]}") #debug
         # print(f" Its row size {len(new_format[0])}")
-        for j in range(total_columns):
-            if seqblock[i,j] == 32: # //Confirmed
-                new_format += " "
-            else: new_format += chr(seqblock[i,j])
+        # if (i == 0):
+        #     original = ""
+        #     for j in range(total_columns):
+        #         original += chr(seqblock[i,j])
+                
+                
+        
+        # if (i != 0):
+  
+        if (i == 0 or i == 1):
+            new_format = ""
+            j=0
+        
+            while j < total_columns:
+                # print(j)
+                                        
+                if stop_found is False:
+                    if (seqblock[i,j] == ord('T') and (j+6) < total_columns):
+                        if (seqblock[i,j+1] == ord('A') and seqblock[i, j+2] == ord('A') and seqblock[i, j+3] == ord('T') and seqblock[i, j+4] == ord('A') and seqblock[i, j+5] == ord('G')):
+                            new_format += 'T'
+                            new_format += 'A'
+                            new_format += 'A'
+                            new_format += 'T'
+                            new_format += 'G'
+                            j = j+6
+                            stop_found = True
+                    if stop_found is False:
+                        new_format += chr(seqblock[i,j])
+                        j+=1
             
-         # Neatly divides data into attributes of CleanSeqBlock object based on row number.
+                elif stop_found is True and i == 1:
+                    tag += chr(seqblock[i,j])
+                    j+=1
+                else:
+                    j = total_columns
+            if (i == 1): 
+                seqblock_parsed.barcode = tag
+                tag_length = len(tag)
+        else:
+            new_format = ""
+            for j in range(total_columns-tag_length):
+                new_format += chr(seqblock[i,j])
+                
+                
         if (i == 0):
             seqblock_parsed.target = new_format
         elif (i == total_rows - 1): # last row is mutations 'x', total_rows is one more than total index
@@ -104,25 +208,10 @@ def seqblock_parser(seqblock):
         else:
             seqblock_parsed.qscores.append(new_format)
     return seqblock_parsed     
-
-class CleanSeqBlock(): 
-    """_summary_
-    Houses six key sequence types from mat file and useable, clean data
-    Object of this type are initialized by seqblock_parser function.
-    """
+class CleanSeqBlock():
     def __init__(self):
-        self.reads_count = 0;
-        self.len = 0;
-        self.barcode = 0; # Super important for future dictionary
-        
-        self.target = 0;
-        
-        self.reads = [];
-        self.qscores = [];
-        
-        self.interp_changes = 0; # what is this again? 
-        self.interp_consensus = 0;
-        self.interp_mutations = 0;
-    
+        self.cov, self.len, self.target, self.interp_changes, self.interp_mutations, self.interp_consensus, self.barcode = 0,0,0,0,0,0,0
+        self.reads, self.qscores = [], []
+
 def pscore(Qscore): 
     return pow(10, -(Qscore-33)/10)
